05232a3ff98d5ed682c7055eaaf9a8c9cf549b41
gaolijun <15801419993@163.com>
2017-09-28 17:27:35 +0800

    添加ignore文件删除.project文件

--- demo1.php
+++ demo1.php
@@ -7,18 +7,17 @@
 <?php 
 echo "Hello World!"; 
 echo "<br>";
-echo $_SERVER['PHP_SELF'];
+echo $_SERVER['PHP_SELF'];   //当前执行脚本的文件名，与 document root 有关
 echo "<br>";
-echo $_SERVER['SERVER_NAME'];
+echo $_SERVER['SERVER_NAME'];    //当前运行脚本所在的服务器的主机名。
 echo "<br>";
-echo $_SERVER['HTTP_HOST'];
+echo $_SERVER['HTTP_HOST'];     //当前请求头中 Host: 项的内容，如果存在的话。
 echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
+echo $_SERVER['HTTP_REFERER'];        //	引导用户代理到当前页的前一页的地址（如果存在）
 echo "<br>";
 echo $_SERVER['HTTP_USER_AGENT'];
 echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
-
+echo $_SERVER['SCRIPT_NAME'];      //包含当前脚本的路径。
 
 ?> 
 
